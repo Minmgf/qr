52553b710a7c491ab9e7fd236abd6e5af210f586
Minmgf <manuelinm2015@gmail.com>
2025-06-19 14:56:37 -0500

    appflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,26 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.qrverifier',
-  appName: 'QR Verifier',
+  appId: 'com.qrverifier.app',
+  appName: 'QR Verifier App',
   webDir: 'dist',
+  bundledWebRuntime: false,
   server: {
-    androidScheme: 'https'
+    androidScheme: 'https',
+    allowNavigation: [
+      'hayplaza.com',
+      'https://hayplaza.com',
+      'https://hayplaza.com/*'
+    ]
+  },
+  android: {
+    allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: true
+  },
+  ios: {
+    scheme: 'QR Verifier App',
+    webContentsDebuggingEnabled: true
   }
 };
 
